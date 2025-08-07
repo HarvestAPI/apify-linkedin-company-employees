9c8d15124c767abd1a935dca043499df281dacc7
xorcuit <xorcuit@gmail.com>
2025-08-08 01:42:32 +0700

    feat: add startPage & takePages params

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,8 @@ interface Input {
   maxItems?: number;
   searchQuery?: string;
   jobTitles?: string[];
+  startPage?: number;
+  takePages?: number;
 }
 
 // Structure of input is defined in input_schema.json
@@ -210,6 +212,8 @@ const scrapeParams: Omit<ScrapeLinkedinSalesNavLeadsParams, 'query'> = {
   overrideConcurrency: profileScraperMode === ProfileScraperMode.EMAIL ? 10 : 8,
   overridePageConcurrency: state.leftItems > 200 ? 2 : 1,
   warnPageLimit: isPaying,
+  startPage: input.startPage,
+  takePages: input.takePages,
 };
 
 let didChargeForStats = false;
