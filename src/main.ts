88d29926c71c684f017e395ece9b039387995e06
xorcuit <xorcuit@gmail.com>
2025-09-10 04:12:34 +0700

    chore: update dependency

--- src/main.ts
+++ src/main.ts
@@ -305,7 +305,7 @@ await scraper.scrapeSalesNavigatorLeads({
       }
     }
     console.info(
-      `Scraped search page ${page}. Found ${data?.elements?.length || 0} profiles on the page.`,
+      `Scraped search page ${page}. Found ${data?.elements?.length} profiles on the page.`,
     );
   },
   addListingHeaders: {
