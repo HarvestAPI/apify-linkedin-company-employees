1a22cb850962c01cbe4a5c1e415399dfcf900bbe
xorcuit <xorcuit@gmail.com>
2026-02-13 06:46:13 +0700

    chore: change concurrency

--- src/main.ts
+++ src/main.ts
@@ -372,8 +372,8 @@ async function runScraper(scraperQuery: SearchLinkedInSalesNavLeadsParams) {
       'x-concurrency': user?.username
         ? isPaying
           ? profileScraperMode === ProfileScraperMode.SHORT
-            ? '1'
-            : '1'
+            ? '2'
+            : '2'
           : '1'
         : (undefined as any),
       'x-request-timeout': '360',
